8451693ecd46fb72876de3ced99a7d0ee823f310
Marcin Kotz <marcin@kotz.pl>
2024-08-27 11:35:32 +0200

    feat: inital comit

---
+++ src/jsMain/kotlin/com/er453r/autodash/App.kt
@@ -0,0 +1,30 @@
+package com.er453r.autodash
+
+import io.kvision.Application
+import io.kvision.CoreModule
+import io.kvision.BootstrapModule
+import io.kvision.BootstrapCssModule
+import io.kvision.html.div
+import io.kvision.html.span
+import io.kvision.module
+import io.kvision.panel.root
+import io.kvision.startApplication
+
+class App : Application() {
+    override fun start() {
+        root("kvapp") {
+            div("Hello world")
+            span("Hello world!")
+        }
+    }
+}
+
+fun main() {
+    startApplication(
+        ::App,
+        module.hot,
+        BootstrapModule,
+        BootstrapCssModule,
+        CoreModule
+    )
+}
